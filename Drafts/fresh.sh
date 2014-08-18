3a2560dba6efa7d1e5faf148c5ba81fb522d44ad
taxbender <akeene@gmail.com>
2014-08-17 23:37:57 -0500

    Update fresh.sh

--- Drafts/fresh.sh
+++ Drafts/fresh.sh
@@ -4,54 +4,57 @@
 #   I use putty to connect to my server and only add ssh-server during the initial 
 #   installation process. The scipt is then copied to the default user's home directory
 #   then run as default user. 
-#
+# This script should be run as the root, e.g. sudo $user
 # Version 0.2 (Still testing)
 #
 
 # Script Variables (Default included)
   
+  # Debug option
+  debug_me="y"
+
   # Install options
-  net_bond = "y"                      # Bond interfaces?
-  email = "y"                         # Install / config sSMTP mail
-  git = "y"                           # Install / config Git; configs for flexget/transmission live here
-  deluge = "n"                        # Install / config deluged and deluge-web; includes deluge user/group config
-  transmission = "n"                  # Install / config Transmission; includes transmission user/group config
-  flexget = "y"                       # Install / config FlexGet;
+  net_bond="y"                        # Bond interfaces?
+  email="y"                           # Install / config sSMTP mail
+  git="y"                             # Install / config Git; configs for flexget/transmission live here
+  deluge="n"                          # Install / config deluged and deluge-web; includes deluge user/group config
+  transmission="n"                    # Install / config Transmission; includes transmission user/group config
+  flexget="y"                         # Install / config FlexGet;
   mounnts "n"                         # Create mount points; Include nfs-client install
-  owncloud = "y"                      # Install / config OwnCloud
-  vnc = "n"                           # Install / config VNC desktop
+  owncloud="y"                        # Install / config OwnCloud
+  vnc="n"                             # Install / config VNC desktop
   
   # Bonded interface variables
   Interfaces=( "eth0" "eth1" )	      # Bond interfaces / network adapters
-  BondName = "bond0"                  # Bond interface name
-  BondIP = 192.168.10.2       	      # Bond interface IP
-  BondGateway = 192.168.10.10         # Bond interface gateway
-  BondNetmask = 255.255.255.0         # Bond interface netmask
-  BondMode = "balance-rr"             # Bond mode; balance-rr provides load balancing and fault tolerance
+  BondName="bond0"                    # Bond interface name
+  BondIP="192.168.10.2"       	      # Bond interface IP
+  BondGateway="192.168.10.10"         # Bond interface gateway
+  BondNetmask="255.255.255.0"         # Bond interface netmask
+  BondMode="balance-rr"               # Bond mode; balance-rr provides load balancing and fault tolerance
 
   #SMTP mail variables
-  email_address = "email@gmail.com"
-  email_password = "email password"
-  send_email = "email address you receive email at"
+  email_address="email@gmail.com"
+  email_password="password"
+  send_email="send@gmail.com"
   
   # Git variables
-  git_user = "username"               # Git username
-  git_email = "email"                 # Git email address for username
+  git_user="username"                 # Git username
+  git_email="email"                   # Git email address for username
   
   # VNC variables
   	# None yet
   	
   	
 # Update Repositories / Upgrade System
-sudo apt-get -y --force-yes update
-sudo apt-get -y --force-yes upgrade  
+apt-get -y --force-yes update
+apt-get -y --force-yes upgrade  
 
 
 ### Install / config scripts ###  
 
   
 ########## Network Bonding ######################
-if [ $net_bond = Y ]
+if [ $net_bond = "y" ]
   then
     # Install bonding enslave program
     sudo apt-get ifenslave
@@ -65,17 +68,17 @@ if [ $net_bond = Y ]
     # Copy original interfaces file
     sudo cp /etc/network/interface /etc/network/interface.original
     # Create interface file for the bonded interface
-    cat >	/etc/network/interface <<"EOF"
-      line 1, auto $BondName
-      line 2, iface $BondName inet static
-      line 3,   address $BondIP
-      line 4,   gateway $BondGateway
-      line 5,   netmask $BondNetmask
-      line 6,   bond-mode $BondMode
-      line 7,   slave
-      line 8,   bond-miimon 100
-      line 9,   bond-updelay 200
-      line 10,   bond-downdelay 200
+    cat >	/etc/network/interface << EOF
+	auto $BondName
+	iface $BondName inet static
+	  address $BondIP
+	  gateway $BondGateway
+	  netmask $BondNetmask
+	  bond-mode $BondMode
+	  slave
+	  bond-miimon 100
+	  bond-updelay 200
+	  bond-downdelay 200
 EOF
     
     # Add the slave interfaces to line 7 of the interface file
@@ -101,25 +104,37 @@ if [ $email = "y" ]
     # Backup original config file
     cp /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.original
     # Create new config file with email settings for gmail
-    cat > /etc/ssmtp/ssmtp.conf <<"EOF"
-      line 1, root=$email_address
-      line 2, mailhub=smtp.gmail.com:587
-      line 3, AuthUser=$email_address
-      line 4, AuthPass=$email_password
-      line 5, UseTLS=YES
-      line 6, UseSTARTTLS=YES
-      line 7, rewriteDomain=gmail.com
-      line 8, hostname=$email_address
-      line 9, FromLineOverride=None
-      line 10, AuthMethod=LOGIN
+    cat > /etc/ssmtp/ssmtp.conf << EOF
+	root=$email_address
+	mailhub=smtp.gmail.com:587
+	AuthUser=$email_address
+	AuthPass=$email_password
+	UseTLS=YES
+	UseSTARTTLS=YES
+	rewriteDomain=gmail.com
+	hostname=$email_address
+	FromLineOverride=None
+	AuthMethod=LOGIN
 EOF
+    
     # sSMTP email password is stored in plain text. Change ownership
     #   and access permissions to the conf file to protect is a bit.
     chown root:mail /etc/ssmtp/ssmtp.conf
     chmod 640 /etc/ssmtp/ssmtp.conf
+
+    # Add user to mail group & reload group assignments
+    usermod -a -G mail $USER
+    exec su -l $USER
+    
     # Send a test email
     echo "Sending a test email. Check your email to ensure config works."
-    ssmtp "Testing sSMTP Configuration" $send_email
+    mail $send_email << EOF
+	To: $send_email
+	From: $email_address
+	Subject: Test Email
+	This is a test email
+EOF
+
   else
     # Do nothing
 fi
@@ -153,20 +168,20 @@ if [ $vnc = y ]
       gnome-session-fallback \
       vnc4server
     # Configure the startp file for x server
-    cat > /home/$USER/.vnc/xstartup <<"EOF"
-      line 1, #!/bin/sh
-      line 2, 
-      line 3, # Uncomment the following two lines for normal desktop:
-      line 4, unset SESSION_MANAGER
-      line 5, #exec /etc/X11/xinit/xinitrc
-      line 6, gnome-session --session=gnome-classic &
-      line 7, 
-      line 8, [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-      line 9, [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-      line 10, xsetroot -solid grey
-      line 11, vncconfig -iconic &
-      line 12, #x-terminal-emulator -geometry 1280x1024+10+10 -ls -title "$VNCDESKTOP Desktop" &
-      line 13, #x-window-manager &
+    cat > /home/$USER/.vnc/xstartup << EOF
+	#!/bin/sh
+       
+	# Uncomment the following two lines for normal desktop:
+	unset SESSION_MANAGER
+	# exec /etc/X11/xinit/xinitrc
+	gnome-session --session=gnome-classic &
+
+	[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+	[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+	xsetroot -solid grey
+	vncconfig -iconic &
+	# x-terminal-emulator -geometry 1280x1024+10+10 -ls -title "$VNCDESKTOP Desktop" &
+	# x-window-manager &
 EOF
     # Configure the startup scripts so VNC does not start at boot
     # Add alias to .bash_aliases for VNC start and stop
@@ -174,7 +189,3 @@ EOF
   else
     # Do nothing
 fi
-
-
-
-
