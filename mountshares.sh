728ec779ce4ecff6a5275642abfdf931c005b47b
taxbender <akeene@gmail.com>
2014-09-09 23:14:25 -0500

    Update mountshares.sh

--- mountshares.sh
+++ mountshares.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # NFS Mounts
-  mount_ip="192.168.10.105"
-  mount_path="/mnt/user"
-  mounts=( "Ghost_Backup" "Media" )
+  export_ip="192.168.10.105"
+  export_path="/mnt/user"
+  export_folders=( "Ghost_Backup" "Media" )
   mount_opts="soft,intr,rsize=8192,wsize=8192"
 
 # Install nfs client app
   apt-get -y install nfs-common
 
 # Create mount points with the same name as NFS folders. 
-  for i in "${mounts[@]}"
+  for i in "${export_folders[@]}"
     do
     # Create mount point
     mkdir /home/$SUDO_USER/$i
@@ -19,10 +19,10 @@
     chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/$i
 
     # Mount NFS folders
-    mount -o $mount_opts $mount_ip:$mount_path/$i /home/$SUDO_USER/$i
+    mount -o $mount_opts $export_ip:$export_path/$i /home/$SUDO_USER/$i
 
     # Add mounts to fstab
-    echo ''                                                                 >> /etc/fstab
-    echo $mount_ip:$mount_path/$i /home/$SUDO_USER/$i nfs $mount_opts 0,0   >> /etc/fstab
+    echo ''                                                                   >> /etc/fstab
+    echo $export_ip:$export_path/$i /home/$SUDO_USER/$i nfs $mount_opts 0,0   >> /etc/fstab
 
   done
