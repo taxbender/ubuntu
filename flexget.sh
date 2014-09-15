37717e9d1192525be6e613847a8848e4aac2a435
taxbender <akeene@gmail.com>
2014-09-14 21:49:13 -0500

    Update and rename Drafts/flexget.sh to flexget.sh

--- flexget.sh
+++ flexget.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #
+
+# User / Group that flexget will run under
+
+flexuser="debian-transmission"
+
 # Install python, python-pip and flexget
   apt-get -y install python2.7
   apt-get -y install python-pip
   pip install flexget
 
 # Add the transmission plugin; Allows FlexGet and Transmission to talk
-  pip install  transmissionrpc
+  pip install transmissionrpc
   
 # Create FlexGet working area
-  sudo -u debian-transmission mkdir /etc/transmission-daemon/.flexget
+  sudo -u $flexuser -g $flexuser mkdir /etc/transmission-daemon/.flexget
   
 # Create empty FlexGet config file
-  sudo -u debian-transmission touch /etc/transmission-daemon/.flexget/config.yml
+  sudo -u $flexuser -g $flexuser touch /etc/transmission-daemon/.flexget/config.yml
