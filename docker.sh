43ce704775b3c4fc62d0c723af026055386935e2
taxbender <akeene@gmail.com>
2014-10-02 21:29:38 -0500

    Update and rename docker to docker.sh

--- docker.sh
+++ docker.sh
@@ -8,16 +8,16 @@ if [ ! -e /usr/lib/apt/methods/https ]; then
 	apt-get install -y apt-transport-https
 fi
 
+# Import the repository key
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
 # Add the repository to your APT sources
 echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
-
-# Then import the repository key
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
 # Install docker
 apt-get update
 apt-get install -y lxc-docker
 
 #
-# Alternatively, just use the curl-able install.sh script provided at https://get.docker.io
+# Alternatively, use the curl-able script provided at https://get.docker.io
 #
