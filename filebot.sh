4bd861c739feb531ee057ee2ad5067cb77baa181
taxbender <akeene@gmail.com>
2015-04-27 23:50:32 -0500

    Update filebot.sh

--- filebot.sh
+++ filebot.sh
@@ -5,9 +5,9 @@
   apt-get purge openjdk*
 
 # Add repositor and install java installer
-  add-apt-repository ppa:webupd8team/java
-  apt-get update
-  apt-get install oracle-java8-installer gdebi
+  add-apt-repository -y ppa:webupd8team/java
+  apt-get -y update
+  apt-get -y install oracle-java8-installer gdebi
 
 # Download filebot and install
   wget sourceforge.net/projects/filebot/files/filebot/FileBot_4.5/filebot_4.5_amd64.deb
